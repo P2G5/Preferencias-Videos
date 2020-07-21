c7afe0a1ec41441fc9e2ae237575942973494b42
Shoukri <saljarjawi@gmail.com>
2020-07-21 14:51:35 +0300

    Navbar

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import SearchBar from '../Searchbar/Searchbar.js';
-import youtube from '../../apis/youtube.js';
-import VideoList from '../VideoList/VideoList.js';
-import VideoDetail from '../VideoDetail/VideoDetail';
-import HomePage from '../HomePage/HomePage.js';
-import './app.css';
+import React from "react";
+import SearchBar from "../Searchbar/Searchbar.js";
+import youtube from "../../apis/youtube.js";
+import VideoList from "../VideoList/VideoList.js";
+import VideoDetail from "../VideoDetail/VideoDetail";
+import HomePage from "../HomePage/HomePage.js";
+import Navbar from "../Navbar/Navbar.js";
+import "./app.css";
 // import cors from 'cors';
 
 class App extends React.Component {
-    state = {
-        videos: [],
-        selectedVideo: null
-    }
-    handleSubmit = async (termFromSearchBar) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: termFromSearchBar
-            }
-        })
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+  handleSubmit = async (termFromSearchBar) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: termFromSearchBar,
+      },
+    });
 
-        this.setState({
-            videos: response.data.items
-        })
-        console.log("this is resp",response);
-    };
-    handleVideoSelect = (video) => {
-        this.setState({selectedVideo: video})
-    }
+    this.setState({
+      videos: response.data.items,
+    });
+    console.log("this is resp", response);
+  };
+  handleVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
-    render() {
-        return (
-            <div className='uicontainer' style={{marginTop: '1em'}}>
-                 <HomePage/>
-                <SearchBar handleFormSubmit={this.handleSubmit}/>
-                <div className='ui grid'>
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo}/>
-                        </div>
-                        <div className="col-4">
-                            <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/>
-                        </div>
-                    </div>
-                </div>
-               
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="uicontainer">
+          <HomePage />
+          <SearchBar handleFormSubmit={this.handleSubmit} />
+          <div className="ui grid">
+            <div className="ui row">
+              <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
+              <div className="col-4">
+                <VideoList
+                  handleVideoSelect={this.handleVideoSelect}
+                  videos={this.state.videos}
+                />
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
