d7717510dd0ec192dfb3a82e428602360c7a9d22
naderaqaoud20 <naderaqaoud2000@gmail.com>
2020-07-22 18:30:41 +0300

    back end for regester done and testing

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -1,18 +1,22 @@
 const UserModel = require('../models/users')
 const bcrypt = require('bcryptjs');
-exports.check_account = (req, res) =>{
-	const {email,password } = req.body;
-     let user = UserModel.findOne({email});
-     if(!user){
-     	res.status(400).send("Register Plz");
-     	console.log("You don't have accout");
-     }
-     const userDoc = new UserModel({email,password});
-     const isMatch = bcrypt.compare(password,userDoc.password);
-     
-    if(!isMatch){
-    	res.status(404).send('Wrong Password')
-    }
-     
+
+ function check_account (req,res){
+   const {email,password } = req.body;
+   let user =  UserModel.findOne({email});
+   if(!user){
+    console.log(user)
+      res.status(400).send("Register Plz")
+      console.log("You don't have an account");
+   }else{
+   //const userDoc = new UserModel({email,password});
+   const isMatch =  bcrypt.compare(password,user.password);
+
+  if(!isMatch){
+     res.status(404).send('Wrong Password')
+     console.log("Please enter your correct password")
+   }else{
+   res.send("correct account")
+}}
  }
- 
+ exports.check_account = check_account ;
---
+++ __tests__/db.test.js
@@ -0,0 +1,47 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+describe('Users',() => {
+    let connection;
+    let database;
+    const users = mongoose.model("test_"+process.env.COLLECTION,mongoose.Schema({
+        name: String,
+        email: String,
+        password: String
+    }));
+
+    beforeAll(async () => {
+       
+        connection = await mongoose.connect('mongodb+srv://videoData:504030asd@cluster0.tellj.mongodb.net/test_'+process.env.DATABASE,{useNewUrlParser: true, useUnifiedTopology: true });
+        db = mongoose.connection;
+        const collection = process.env.COLLECTION;
+        await db.createCollection(collection);
+
+    });
+
+    afterAll(async () => {
+
+        const collection = "test_"+process.env.COLLECTION;
+        await db.dropCollection(collection);
+        await db.dropDatabase();
+        await db.close();
+        await connection.close();
+
+    });
+
+    
+    test("Add Users POST /signup/createUser",async () => {
+
+        const response = await users.create({
+            name: process.env.CUSTOMER_NAME,
+            email: process.env.CUSTOMER_EMAIL,
+            password:process.env.CUSTOMER_PASSWORD
+        });
+        await response.save();
+        expect(response.name).toBe(process.env.CUSTOMER_NAME);
+
+    });
+
+
+});
